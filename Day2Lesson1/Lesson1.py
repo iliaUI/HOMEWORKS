2e6de1739587970f4ae62c92ae7ecefac71f854f
iliaUI <ilikogvenetadze@gmail.com>
2024-01-31 20:13:00 +0400

    first commit

---
+++ Day2Lesson1/Lesson1.py
@@ -0,0 +1,13 @@
+name = "ilia"
+surname = "gvenetadze"
+age = 15
+heigh = 175.3
+want_to_learn_programming = True
+is_stupid = False
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(heigh))
+print(type(want_to_learn_programming))
+print(name + surname)
+print(name + " " + str(age) + surname + str(heigh))
