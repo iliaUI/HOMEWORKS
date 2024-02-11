ac66694048d77792ed962475376a9d5dafd86f39
iliaUI <ilikogvenetadze@gmail.com>
2024-02-11 12:56:20 +0400

    classwork1

--- classwork1/classwork1/classwor1.py
+++ classwork1/classwork1/classwor1.py
@@ -9,4 +9,18 @@ print ("wake up")
 
 print ("am not going to rage this person")
 
+print(13)
+
+print("thirteen")
+
+my_age = 13
+
+print ("a'm " + str(my_age) + " years old")
+
+my_age = my_age + 1
+print("my age after one year is "+ str(("my_age")))
+
+
+
+
 
