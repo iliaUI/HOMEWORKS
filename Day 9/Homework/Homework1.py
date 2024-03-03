d585ed4591fc52a92745517089ec5d30b9bbcca9
iliaUI <ilikogvenetadze@gmail.com>
2024-03-03 14:59:01 +0400

    second commit

---
+++ Day 9/Homework/Homework1.py
@@ -0,0 +1,15 @@
+# and operator 
+
+print(True and False) #False
+print(True and True) #True
+print(False and True) #False
+print(False and False) #False
+print(False and True) #False
+
+# or operator
+
+print(True or True) #True
+print(True or False) #True
+print(False or False) #False
+print(False or True) #True
+print(True or False) #True
---
+++ Day 9/Homework/Homework2.py
@@ -0,0 +1,15 @@
+#True example
+
+print(23 > 2)
+print(2 < 4)
+print(5+3 > 9-3)
+print(2*3 > 1*5)
+print(14//2 > 12//3)
+
+#False example
+
+print(12 < 3)
+print(22 > 12+11)
+print(12-4 > 3+15)
+print(6*8 > 9*9)
+print(72//2*3 < 22*2)
