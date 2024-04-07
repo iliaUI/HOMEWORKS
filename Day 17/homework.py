b89115dfcb464d0b45d2afbae7f2441bee5f0f3e
iliaUI <ilikogvenetadze@gmail.com>
2024-04-07 17:29:47 +0400

    second commit

---
+++ Day 17/homework.py
@@ -0,0 +1,13 @@
+my_list = [42, "hello", 3.14, [1, 2, 3]]
+
+print("Original list:", my_list)
+
+my_list[0] = 100
+
+my_list[1] = "world"
+
+my_list[2] = 6.28
+
+my_list[3] = ['a', 'b', 'c']
+
+print("Modified list:", my_list)
