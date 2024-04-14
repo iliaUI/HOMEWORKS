da84185e9cf95356fbb63a4da057bfd22304ac79
iliaUI <ilikogvenetadze@gmail.com>
2024-04-14 14:06:17 +0400

    second commit

---
+++ Day 19/Classwork/Classwork.py
@@ -0,0 +1,12 @@
+#products = ['apple', 'orange', 'banana', 'mandarine', 'juice']
+#products[0] = "hello world, am ilia"
+#print(products)
+
+#vehicle = 'motorbike'
+#print(vehicle[:])
+
+product = ['apple', 'banana', 'kiwi']
+print(product[:2])
+
+name = ['ilia']
+print(name[1:])
---
+++ Day 19/Homework/Homework2.py
@@ -0,0 +1,3 @@
+proposal = 'hello, how are you today?'
+print(proposal[0:5])
+print(proposal[19:24])
