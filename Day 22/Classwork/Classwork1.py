4fc6ff062f3e9f273d89f72f6e87b38e15482cd2
iliaUI <ilikogvenetadze@gmail.com>
2024-04-28 07:56:36 +0400

    second commit

---
+++ Day 22/Classwork/Classwork1.py
@@ -0,0 +1,13 @@
+def Multiply(a, b):
+    return a * b
+
+print(Multiply(10, 5))
+print(Multiply(15, 20))
+
+
+
+def sayHi(name):
+    return "Hello " + name, "How are you?"
+
+print(sayHi("Ilia"))
+
