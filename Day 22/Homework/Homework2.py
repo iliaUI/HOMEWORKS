b7c4f495940a8af226093921ba9d8d4ab8af618b
iliaUI <ilikogvenetadze@gmail.com>
2024-04-28 11:20:21 +0400

    second commit

---
+++ Day 22/Homework/Homework2.py
@@ -0,0 +1,7 @@
+def rectangle_area(length, height):
+    return length * height
+
+# Example usage
+length = 5
+height = 3
+print("Area of rectangle with length", length, "and height", height, "is:", rectangle_area(length, height))
---
+++ Day 22/Homework/Homework3.py
@@ -0,0 +1,6 @@
+def generate_string():
+    return "Goa best " * 10
+
+# Example usage
+result = generate_string()
+print(result)
