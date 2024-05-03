80a01b31934ba4278bc41aa641ba51768b684f2d
iliaUI <ilikogvenetadze@gmail.com>
2024-05-03 16:34:37 +0400

    second commit

--- Day 22/Classwork/Classwork1.py
+++ Day 22/Classwork/Classwork1.py
@@ -1,13 +1,5 @@
-def Multiply(a, b):
-    return a * b
-
-print(Multiply(10, 5))
-print(Multiply(15, 20))
 
 
-
-def sayHi(name):
-    return "Hello " + name, "How are you?"
-
-print(sayHi("Ilia"))
-
+def bmi(weight, height):
+    index = weight / (height * height)
+    print(index)
