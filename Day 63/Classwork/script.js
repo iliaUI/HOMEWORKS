a463b81f1713c951d2f53862129823a97adeecf9
iliaUI <iliagvenetadze14@gmail.com>
2024-10-01 10:52:49 -0700

    second commit

---
+++ Day 63/Classwork/script.js
@@ -0,0 +1,11 @@
+console.log('typing')
+
+//math operations
+
+console.log(5 + 8)
+console.log(7 - 3)
+
+
+let height = 14;
+let width = 8;
+console.log(height*width)
---
+++ Day 63/Homework/script.js
@@ -0,0 +1,2 @@
+let name = ("ILya")
+const surename = ("Gvenetadeze")
