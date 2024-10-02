bc9365e819e648194caa921aa044b23a1791517f
iliaUI <iliagvenetadze14@gmail.com>
2024-10-02 08:04:30 -0700

    second commit

---
+++ Day 61/Homework/script.js
@@ -0,0 +1,12 @@
+let q = 10 + 6
+let w = 10 + 3
+let e = 10 + 1
+let r = 10 + 5
+let t = 10 + 5
+let y = 10 + 5
+let u = 10 + 5
+let i = 10 + 5
+let o = 10 + 1
+let p = 10 + 6
+
+console.log (q, w, e, r, t, y, u, i, o, p)
